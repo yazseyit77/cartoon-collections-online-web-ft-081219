edbe6b6508a57a2cdc957c5cb30587a81773c555
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 13:09:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,13 @@ def summon_captain_planet(veggies)# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  if calls.length is <= 4
+    return false
+  else
+    return true
+  end
 end
 
 def find_the_cheese# code an argument here
