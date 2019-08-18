34c768f50b135ab33206f06d04b421b9951a4b80
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 13:01:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,18 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  count = 1
+  dwarves.each do |dwarf|
+    puts "#{count}. *#{dwarf}\n"
+    count += 1
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
+  array = []
+  veggies.each do |veggie| veggie.capitalize
+    array << "#{veggie}!"
+  end
   # Your code here
 end
 
